649686aac21e05c521e9038964ae288cc852f21f
Daniel Coloma <danielcoloma@gmail.com>
2022-04-07 16:23:28 +0200

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,6 @@
+test("Valid Plate", function (assert) {
+  assert.equal(isValidPlate("0000BBB"), true, "Lower Boundary");
+});
+test("Invalid Plate", function (assert) {
+  assert.equal(isValidPlate("0000AAA"), false, "Vowels as letters");
+});
